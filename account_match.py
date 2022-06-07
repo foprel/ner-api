8af93a91bdbb28f39ef628e64be85201def0f16a
Frank Oprel <frank.oprel@vodafoneziggo.com>
2022-06-07 12:09:34 +0200

    First commit

---
+++ account_match.py
@@ -0,0 +1,75 @@
+import csv
+from fuzzywuzzy import fuzz
+from nltk.tokenize import word_tokenize
+import re
+import nltk
+# nltk.download('punkt')
+
+
+def get_accounts(filename):
+    with open(filename, 'r', newline="", encoding="utf-8") as f:
+        f_reader = csv.reader(f, delimiter=',', quotechar="\"")
+        accounts = []
+        for row in f_reader:
+            # preprocess accounts here
+            account_name = row[0]
+            accounts.append([account_name])
+    return accounts
+
+
+def get_stopwords(filename, n=20):
+    with open(filename, 'r', newline="", encoding="utf-8") as f:
+        f_reader = csv.reader(f, delimiter=",", quotechar="\"")
+        word_count = {}
+        for row in f_reader:
+            account_name = clean(row[0])
+            tokens = word_tokenize(account_name)
+            for token in tokens:
+                if token not in word_count:
+                    word_count[token] = 1
+                elif token in word_count:
+                    word_count[token] += 1
+        stopwords_dict = {k: v for k, v in sorted(word_count.items(), key=lambda item: item[1], reverse=True)}
+        stopwords = [keys for keys, _ in stopwords_dict.items()][:n]
+    return stopwords
+
+
+def clean(input_str):
+    input_str = input_str.lower()
+    output_str = re.sub(r"[^A-Za-z0-9\s]+", "", input_str)
+    return output_str
+
+
+def match(str2match, accounts, stopwords, u=95, keep_old_value=True):
+    rank_list = []
+
+    tokens = word_tokenize(clean(str2match))
+    for idx1, token in enumerate(tokens):
+        if token in stopwords:
+            tokens[idx1] = ""
+    tokens = [x for x in tokens if x]
+    str2match_cleaned = " ".join(tokens)
+
+    for account in accounts:
+        account_name = word_tokenize(clean(account[0]))
+        for idx2, token in enumerate(account_name):
+            if token in stopwords:
+                account_name[idx2] = ""
+        account_name = [x for x in account_name if x]
+        account_name = " ".join(account_name)
+
+        token_sort_ratio = fuzz.token_sort_ratio(str2match_cleaned, account_name)
+        rank_list.append([token_sort_ratio])
+
+    score = max(rank_list)[0]
+
+    if score >= u:
+        best_match_index = rank_list.index(max(rank_list))
+        best_match = accounts[best_match_index][0]
+    else:
+        if keep_old_value:
+            best_match = str2match
+        else:
+            best_match = ""
+
+    return best_match
---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, jsonify, request, abort, make_response
+from account_match import get_accounts, get_stopwords, match
+import jwt
+import datetime
+import os
+from functools import wraps
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['USER_NAME'] = os.environ.get('USER_NAME')
+app.config['PASSWORD'] = os.environ.get('PASSWORD')
+app.config['IP_RANGES'] = os.environ.get('IP_RANGE').split(",")
+
+filename = "entity_names.csv"
+accounts = get_accounts(filename)
+stopwords = get_stopwords(filename)
+
+
+def validate_token(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        token = request.headers.get("token")
+        if not token:
+            return jsonify({"message": "Token required!"}), 403
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except Exception:
+            return jsonify({"message": "Token is invalid!"}), 403
+        return func(*args, **kwargs)
+    return decorated
+
+
+def validate_ip(func):
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        ip = request.headers.get('X-Forwarded-For', request.remote_addr)
+
+        if not ip:
+            return jsonify({"message": "Unable to identify ip address"}), 403
+        try:
+            if ip not in app.config["IP_RANGES"]:
+                raise Exception("Ip address is invalid")
+        except Exception:
+            return jsonify({"message": "Ip address is invalid"}), 403
+        return func(*args, **kwargs)
+    return decorated
+
+
+@app.route('/login', methods=['GET'])
+def login():
+    auth = request.authorization
+    if auth and auth.password == app.config['PASSWORD'] and auth.username == app.config['USER_NAME']:
+        token = jwt.encode({'user': auth.username, 'exp': datetime.datetime.utcnow() + datetime.timedelta(days=365)}, app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('UTF-8')})
+
+    return make_response('Could not verify!', 401, {'WWW-Authenticate': 'Basic realm="Login required"'})
+
+
+@app.route('/api/account_name_client/', methods=['POST'])
+@validate_token
+def account_name_client():
+    if not request.json.get("account_name_client"):
+        abort(400)
+
+    account_name_client = request.json.get("account_name_client")
+    account_name_client_norm = match(account_name_client, accounts, stopwords, u=95, keep_old_value=True)
+
+    print(f"{account_name_client} --> {account_name_client_norm}")
+
+    return jsonify({
+        "accountNameClient": account_name_client_norm
+    })
